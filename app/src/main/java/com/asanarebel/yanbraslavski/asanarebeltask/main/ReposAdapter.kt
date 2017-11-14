19bde6e7c3d21a9a4cd5cc5fb9b85e1fe3690503
Yan Braslavsky <yan.braslavsky@home24.de>
2017-11-14 12:05:26 +0100

    Updated list item to present more data

--- app/src/main/java/com/asanarebel/yanbraslavski/asanarebeltask/main/ReposAdapter.kt
+++ app/src/main/java/com/asanarebel/yanbraslavski/asanarebeltask/main/ReposAdapter.kt
@@ -4,9 +4,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.asanarebel.yanbraslavski.asanarebeltask.R
 import com.asanarebel.yanbraslavski.asanarebeltask.api.models.responses.GithubRepoResponseModel
+import com.squareup.picasso.Picasso
 
 /**
  * Created by yan.braslavski on 11/14/17.
@@ -26,6 +28,9 @@ class ReposAdapter(private val mDataItems: List<GithubRepoResponseModel>,
 
             //update question title
             holder.mRepoNameTextView.text = item.name
+            holder.mRepoDescriptionTextView.text = item.description
+            holder.mRepoForksCountTextView.text = "${item.forks_count}"
+            Picasso.with(holder.mAvatarImage.context).load(item.owner.avatar_url).placeholder(R.drawable.ic_launcher_background).into(holder.mAvatarImage)
             holder.itemView.setOnClickListener({
                 mListener?.invoke(item)
             })
@@ -38,6 +43,9 @@ class ReposAdapter(private val mDataItems: List<GithubRepoResponseModel>,
 
     inner class ViewHolder(mView: View) : RecyclerView.ViewHolder(mView) {
         val mRepoNameTextView: TextView = mView.findViewById(R.id.repo_name_text_view)
+        val mRepoDescriptionTextView: TextView = mView.findViewById(R.id.repo_description_text_view)
+        val mRepoForksCountTextView: TextView = mView.findViewById(R.id.forks_count_text_view)
+        val mAvatarImage: ImageView = mView.findViewById(R.id.avatar_image_view)
         var mItem: GithubRepoResponseModel? = null
     }
 }
